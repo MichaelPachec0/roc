632a68775feafdfca511d876e2fa552b57e2ddff
Michael Pacheco <git@michaelpacheco.org>
2023-08-11 16:54:41 -0700

    RoC 2023: Day-2: Example completed.
    * There might be a better way to complete this. For now this is it.
    
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- day-2/rust/src/lib.rs
+++ day-2/rust/src/lib.rs
@@ -1,15 +1,31 @@
 fn core(input: &str) -> Option<i32> {
-    input
-        .split('x')
-        .into_iter()
-        // NOTE: dont care about errors, just bubble up a none for now.
-        .map(|x: &str| match i32::from_str_radix(x, 10).ok() {
-            Some(val) => val,
-            None => return None,
-        })
-        .collect();
-    Some(0)
+    let mut present_box: [i32; 3] = [0; 3];
+    let mut min = i32::MAX;
+    for (i, str) in input.split('x').into_iter().enumerate() {
+        let side = i32::from_str_radix(str, 10).ok()?;
+        // WARN: lets be "safe", since there are only 3 possible entries
+        if i < 3 {
+            present_box[i] = side;
+        } else {
+            return None;
+        }
+    }
+    // NOTE: do half of the calcuation for finding the surface area of the box.
+    present_box = [
+        present_box[0] * present_box[1],
+        present_box[1] * present_box[2],
+        present_box[0] * present_box[2],
+    ];
+    let ret = present_box.into_iter().fold(0, |acc, side| {
+        if side < min {
+            min = side
+        };
+        // NOTE: here we do the last half of the surface area calculation.
+        acc + (side * 2)
+    });
+    Some(ret + min)
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -18,6 +34,26 @@ mod tests {
     use std::io::{BufRead, BufReader};
     use std::time::{Duration, Instant};
     use std::{env, fs};
+
+    #[test]
+    fn example_test() {
+        let tests: [&str; 2] = ["2x3x4", "1x1x10"];
+        let solutions: [i32; 2] = [58, 43];
+        tests
+            .into_iter()
+            .enumerate()
+            .zip(solutions)
+            .for_each(|((i, str), sol)| {
+                let check = core(str);
+                println!("ITER {i}");
+                match check {
+                    Some(actual) => {
+                        assert_eq!(actual, sol, "ERROR: EXPECTED {sol} NEQL ACTUAL: {actual}");
+                    }
+                    None => panic!("ERROR in processing {str}"),
+                }
+            })
+    }
 
     /// Helper function to return a File Buffer. Used to isolate imperative code from the
     /// codebase
